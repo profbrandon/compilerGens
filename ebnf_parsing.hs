d8bcdb4481202de451db9ac58f28738988fc58be
profbrandon <brandonium1999@outlook.com>
2017-05-29 18:43:12 -0600

    Update ebnf_parsing.hs

--- ebnf_parsing.hs
+++ ebnf_parsing.hs
@@ -1,15 +1,33 @@
-// Library for parsing ebnf formatted files
-
-import Data.Char
+{- Library for parsing ebnf formatted files -}
 
 module EBNF where
 
+import Data.Char
+
 data Token = Token { tType::String
                    , tvalue::String
-                   }
+                   } deriving (Show)
                    
 lexEBNF :: String -> [Token]
-lexEBNF "" = (Token "EOT" "$"):[]
-lexEBNF (x:xs) | x == ' ' = lexEBNF xs
-               | x == '|' = (Token "Alt" "|"):lexEBNF xs
-               | otherwise = []
+lexEBNF []     = (Token "EOT" "$"):[]
+lexEBNF (x:xs) | isAlphaNum x = []
+lexEBNF (x:xs) = case x of
+                    ' ' -> lexEBNF xs
+                    '=' -> back "DEF" "="
+                    ',' -> back "CON" ","
+                    ';' -> back "TER" ";"
+                    '|' -> back "ALT" "|"
+                    '[' -> back "LSB" "["
+                    ']' -> back "RSB" "]"
+                    '{' -> back "LCB" "{"
+                    '}' -> back "RCB" "}"
+                    '(' -> back "LP" "("
+                    ')' -> back "RP" ")"
+                    '\"' -> back "DQ" "\""
+                    '\'' -> back "SQ" "\'"
+                    '*' -> back "COM" "*"
+                    '?' -> back "SSE" "?"
+                    '-' -> back "EXC" "-"
+                    _ -> error "incorrect grammar format"
+                    where back = \x y -> (Token x y):(lexEBNF xs)
+
