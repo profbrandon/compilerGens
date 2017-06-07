06e369fada336cbbeb4167e5911c7101aca72c37
Brandon D. Harrington <brandonium1999@outlook.com>
2017-06-07 16:51:21 -0600

    attempt to make more general and efficient

--- ebnf_parsing.hs
+++ ebnf_parsing.hs
@@ -8,17 +8,25 @@ data Token = Token { tType::String
                    , tvalue::String
                    } deriving (Show)
 
-idHandler :: (String, String) -> [Token]
-
 ops   = ['=', ',', ';', '|', '[', ']', '{', '}', '(', ')', '\'', '\"', '*', '?', '-']
 ids   = ["Definition", "Concatination", "Termination", "Alternation", "Left Optional"
         , "Right Optional", "Left Repetition", "Right Repetition", "Left Parenthesis"
         , "Right Parenthesis", "Single Quote Literal", "Double Quote Literal"
         , "Half Comment", "Special Sequence", "Exception"]
 
+idHandler :: String -> String -> [Token]
+idHandler [] [] = error "No identifier found"
+idHandler [] name = [Token "Identifier" (reverse name)]
+idHandler (x:xs) name 
+	| isAlphaNum x || x == '_' || x == ' ' = idHandler xs (x:name)
+	| otherwise = (Token "Identifier" (reverse name)):lexEBNF (x:xs)
+
 lexEBNF :: String -> [Token]
-lexEBNF [] = (Token "EOT" "$"):[]
-lexEBNF (x:xs) | isAlphaNum x = []
-               | elem x ops = let left = ids !! (elemIndex x ops) in (uncurry (Token) (left, [x])):lexEBNF xs
-               | otherwise = error "Incorrect EBNF Format"
-               where z = zip ids ops
+lexEBNF [] = [Token "EOT" "$"]
+lexEBNF (x:xs)
+	| x == ' ' = lexEBNF xs
+	| isAlphaNum x || x == '_' = idHandler xs [x]
+    | elem x ops = 
+        (Token t [v]):lexEBNF xs
+    | otherwise = error "Incorrect EBNF Format"
+    where (t, v) = head (filter (\k -> snd k == x) (zip ids ops))
