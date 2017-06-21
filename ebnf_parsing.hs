047fb9032af9d8ad1af989cfc6d1b7e077bb292c
Brandon D. Harrington <brandonium1999@outlook.com>
2017-06-21 15:26:41 -0600

    Added comments and minor tweaks to fix escape sequences

--- ebnf_parsing.hs
+++ ebnf_parsing.hs
@@ -14,22 +14,23 @@ ids   = ["Definition", "Concatination", "Termination", "Alternation", "Left Opti
         , "Right Parenthesis", "Single Quote Literal", "Double Quote Literal"
         , "Half Comment", "Special Sequence", "Exception"]
 
-
 remfrntSpace :: String -> String --Jason did this :D
 remfrntSpace [] = []
 remfrntSpace (x:xs) 
-  | x == ' ' = remfrntSpace xs
+  | isSpace x = remfrntSpace xs
   | otherwise = x:xs
 
 remBackSpace :: String -> String
 remBackSpace = reverse . remfrntSpace . reverse
 
+-- trimSpace removes whitespace from the front and back of text
 trimSpace :: String -> String
 trimSpace = remfrntSpace . remBackSpace
 
+-- notChar determines if two characters are not equivalent (escape characters produce True)
 notChar :: Char -> Char -> String -> Bool
 notChar c1 c2 [] = c1 /= c2
-notChar c1 c2 (x:xs) = c1 /= c2 || x == '\\'
+notChar c1 c2 l = c1 /= c2 ||(last l) == '\\'
 
 idHandler :: String -> String -> [Token]
 idHandler = sHandler (\c s -> isAlphaNum c || c == '_' || c == ' ') True "Identifier"
@@ -43,6 +44,10 @@ sqHandler = sHandler (notChar '\'') False "Single Quote Literal"
 dqHandler :: String -> String -> [Token]
 dqHandler = sHandler (notChar '\"') False "Double Quote Literal"
 
+{- sHandler handles sequences of characters.  The first argument is a function that
+   determines if a character should be appended to the sequence.  The second argument
+   determines whether the last character needs to be parsed.  The third is a string
+   representing the token type -}
 sHandler :: (Char -> String -> Bool) -> Bool -> String -> String -> String -> [Token]
 sHandler _ _ [] _ _ = error "No type supplied to token"
 sHandler _ _ t [] [] = error ("No " ++ t ++ " found")
